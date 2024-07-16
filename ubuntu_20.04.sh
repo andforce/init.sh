35b22e0e0b549d43e73e5ab487891837bb46c4e7
wangdiyuan <861199@163.com>
2024-07-16 17:58:33 +0800

    config java

--- ubuntu_20.04.sh
+++ ubuntu_20.04.sh
@@ -85,3 +85,17 @@ python_config() {
 }
 
 python_config
+
+java_config() {
+    curl -s "https://get.sdkman.io" | bash
+    # source "$HOME/.sdkman/bin/sdkman-init.sh"
+    # 把 "$HOME/.sdkman/bin/sdkman-init.sh" 放到 ~/.zshrc 中
+    echo "source \"$HOME/.sdkman/bin/sdkman-init.sh\"" >> ~/.zshrc
+    echo "配置完成，请重新登录或者执行 source ~/.zshrc"
+    # sdk list java  # 列出所有可用的JDK版本
+    # sdk install java 11.0.11.hs-adpt  # 安装特定版本的JDK
+    # sdk use java 11.0.11.hs-adpt  # 切换到特定版本的JDK
+    # sdk install java 11.0.14.1-jbr
+}
+
+java_config
