6e872f36f366b1e01cb9ebd66399a5cbff5276a3
andforce <andforce@users.noreply.github.com>
2024-07-20 10:37:19 +0800

    Update pi2w.sh

--- pi2w.sh
+++ pi2w.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 sudo apt install git net-tools -y
+sudo systemctl stop ModemManager
+sudo systemctl disable ModemManager.service
+sudo systemctl list-dependencies multi-user.target | grep Modem
