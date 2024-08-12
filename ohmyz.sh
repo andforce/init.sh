247f002398bfb987674f75c249d69c6c73273f99
dy <86118@163.com>
2024-08-12 11:43:53 +0800

    android sdk

--- ohmyz.sh
+++ ohmyz.sh
@@ -1,38 +1,6 @@
 #!/usr/bin/sh
 
-# 强制交互式模式
-if [ -z "$PS1" ]; then
-    export PS1="ssh-remote$ "
-fi
-
-install_git() {
-  # 检查是否安装了 git
-    if [ -z "$(command -v git)" ]; then
-        echo "安装 git"
-        sudo apt-get install -y git
-    else
-        echo "git 已安装"
-    fi
-
-    echo "配置 git"
-    # 等待用户输入用户名和邮箱
-    echo "请输入用户名，git config --global user.name: "
-    read -r username
-    echo "请输入邮箱，git config --global user.email: "
-    read -r email
-    git config --global user.name "$username"
-    git config --global user.email "$email"
-    git config --global core.editor vim
-    git config --global alias.st status
-    git config --global alias.co checkout
-    git config --global alias.ci commit
-    git config --global alias.br branch
-    git config --global pager.branch false
-}
-
 install_zsh() {
-    install_git
-
     echo "安装 ohmyzsh，官方网站：https://ohmyz.sh"
     # 检查是否安装了 zsh
     if [ -z "$(command -v zsh)" ]; then
