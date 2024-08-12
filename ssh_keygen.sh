958725a7c4174cdeb430f1cb0f5741e77171b341
dy <86118@163.com>
2024-08-12 14:08:35 +0800

    keygen

--- ssh_keygen.sh
+++ ssh_keygen.sh
@@ -2,38 +2,44 @@
 
 # 系统配置
 ssh_keygen() {
-    eval="eval \"$(ssh-agent -s)\""
 
-    if ! grep -Fxq "$eval" $HOME/.zshrc; then
+
+    if ! grep -Fxq "eval \"$(ssh-agent -s)\"" $HOME/.zshrc; then
         # 如果没有找到，则追加到文件中
-        echo "$eval" | sudo tee -a $HOME/.zshrc
-    else
-        echo "条目已存在，无需追加。"
-    fi
-
-    if ! grep -Fxq "$eval" $HOME/.bashrc; then
-            # 如果没有找到，则追加到文件中
-        echo "$eval" | sudo tee -a $HOME/.bashrc
+        echo 'if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+            eval "$(ssh-agent -s)"
+        fi' >> ~/.zshrc
     else
         echo "条目已存在，无需追加。"
     fi
 
 
-    add="ssh-add $HOME/.ssh/id_ed25519"
-
-    if ! grep -Fxq "$add" $HOME/.zshrc; then
+    if ! grep -Fxq "eval \"$(ssh-agent -s)\"" $HOME/.bashrc; then
         # 如果没有找到，则追加到文件中
-        echo "$add" | sudo tee -a $HOME/.zshrc
+        echo 'if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+            eval "$(ssh-agent -s)"
+        fi' >> ~/.bashrc
     else
         echo "条目已存在，无需追加。"
     fi
 
-    if ! grep -Fxq "$add" $HOME/.bashrc; then
-            # 如果没有找到，则追加到文件中
-        echo "$add" | sudo tee -a $HOME/.bashrc
-    else
-        echo "条目已存在，无需追加。"
-    fi
+
+
+#    add="ssh-add $HOME/.ssh/id_ed25519"
+#
+#    if ! grep -Fxq "$add" $HOME/.zshrc; then
+#        # 如果没有找到，则追加到文件中
+#        echo "$add" | sudo tee -a $HOME/.zshrc
+#    else
+#        echo "条目已存在，无需追加。"
+#    fi
+#
+#    if ! grep -Fxq "$add" $HOME/.bashrc; then
+#            # 如果没有找到，则追加到文件中
+#        echo "$add" | sudo tee -a $HOME/.bashrc
+#    else
+#        echo "条目已存在，无需追加。"
+#    fi
 
 }
 
