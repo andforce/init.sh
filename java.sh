9adef3791ef9670da72eb26608a0a7e8657278bf
dy <86118@163.com>
2024-08-12 12:04:01 +0800

    java

--- java.sh
+++ java.sh
@@ -5,6 +5,8 @@ java_config() {
     # source "$HOME/.sdkman/bin/sdkman-init.sh"
     # 把 "$HOME/.sdkman/bin/sdkman-init.sh" 放到 ~/.zshrc 中
     echo "source \"$HOME/.sdkman/bin/sdkman-init.sh\"" >> ~/.zshrc
+    # shellcheck disable=SC3046
+    source "$HOME/.sdkman/bin/sdkman-init.sh"
     echo "配置完成，请重新登录或者执行 source ~/.zshrc"
     # sdk list java  # 列出所有可用的JDK版本
     # sdk install java 11.0.11.hs-adpt  # 安装特定版本的JDK
