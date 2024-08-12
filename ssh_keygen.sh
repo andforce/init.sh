3d3927dfc01e2ca7986055f8b77408d292b553dd
dy <86118@163.com>
2024-08-12 14:14:12 +0800

    ssh-keygen

--- ssh_keygen.sh
+++ ssh_keygen.sh
@@ -6,22 +6,24 @@ ssh_keygen() {
 
     if ! grep -Fxq "eval \"$(ssh-agent -s)\"" $HOME/.zshrc; then
         # 如果没有找到，则追加到文件中
-        echo 'if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+        {
+          if ! pgrep -u "$USER" ssh-agent > /dev/null; then
             eval "$(ssh-agent -s)"
-        fi' >> ~/.zshrc
+          fi
+        } >> ~/.zshrc
     else
         echo "条目已存在，无需追加。"
     fi
 
 
-    if ! grep -Fxq "eval \"$(ssh-agent -s)\"" $HOME/.bashrc; then
-        # 如果没有找到，则追加到文件中
-        echo 'if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-            eval "$(ssh-agent -s)"
-        fi' >> ~/.bashrc
-    else
-        echo "条目已存在，无需追加。"
-    fi
+#    if ! grep -Fxq "eval \"$(ssh-agent -s)\"" $HOME/.bashrc; then
+#        # 如果没有找到，则追加到文件中
+#        echo 'if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+#            eval "$(ssh-agent -s)"
+#        fi' >> ~/.bashrc
+#    else
+#        echo "条目已存在，无需追加。"
+#    fi
 
 
 
