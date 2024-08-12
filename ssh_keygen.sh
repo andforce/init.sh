49f06c0e1ba08aee04db2253d3a0061f8b984b1a
dy <86118@163.com>
2024-08-12 14:23:31 +0800

    ssh-keygen

--- ssh_keygen.sh
+++ ssh_keygen.sh
@@ -29,14 +29,14 @@ fi"
 
 
 
-#    add="ssh-add $HOME/.ssh/id_ed25519"
-#
-#    if ! grep -Fxq "$add" $HOME/.zshrc; then
-#        # 如果没有找到，则追加到文件中
-#        echo "$add" | sudo tee -a $HOME/.zshrc
-#    else
-#        echo "条目已存在，无需追加。"
-#    fi
+    add="ssh-add $HOME/.ssh/id_ed25519"
+
+    if ! grep -Fxq "$add" $HOME/.zshrc; then
+        # 如果没有找到，则追加到文件中
+        echo "$add" | sudo tee -a $HOME/.zshrc
+    else
+        echo "条目已存在，无需追加。"
+    fi
 #
 #    if ! grep -Fxq "$add" $HOME/.bashrc; then
 #            # 如果没有找到，则追加到文件中
